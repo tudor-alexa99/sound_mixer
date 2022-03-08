26d10f2897579895d87fbb002b8cf57a30fc5c9a
Radu <radu.zubascu@vancopayments.com>
2022-03-08 17:08:40 +0200

    Initial Commit

---
+++ SoundMix/SoundMixApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundMixApp.swift
+//  SoundMix
+//
+//  Created by Tudor Alexa on 08.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SoundMixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
