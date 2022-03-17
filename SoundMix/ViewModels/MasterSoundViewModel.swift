aa81be4dca43284cb367b1f5062a5d282ec22c40
tudor_alexa <tudor.alexa99@gmail.com>
2022-03-17 18:02:14 +0200

    Add mixer node to play 2 files at the same time

--- SoundMix/ViewModels/MasterSoundViewModel.swift
+++ SoundMix/ViewModels/MasterSoundViewModel.swift
@@ -27,33 +27,55 @@ class MasterSoundViewModel: NSObject, Identifiable, ObservableObject, AVAudioPla
     }
 
     // MARK: - Prepare audio engine and files
-    
+
     private func setupAudio() {
         for audioVM in audioList.audioList {
             totalTimelineAudioLength += audioVM.audioLengthSeconds
         }
         print("Total audio playback time: \(totalTimelineAudioLength)")
     }
-    
+
     // MARK: - Audio playback
 
     func playAllAudioTracks() {
-        let audioFile = audioList.audioList[0].audioFile
-        let playerNode = AVAudioPlayerNode()
+        let audioFile1 = audioList.audioList[0].audioFile
+        let audioFile2 = audioList.audioList[1].audioFile
+
+        let playerNode1 = AVAudioPlayerNode()
+        let playerNode2 = AVAudioPlayerNode()
+
+        // create a mixer node to combine the existing nodes together
+        let mixerNode = AVAudioMixerNode()
 
         // attach the player node to the audio engine
-        audioEngine.attach(playerNode)
+        audioEngine.attach(playerNode1)
+        audioEngine.attach(playerNode2)
+        audioEngine.attach(mixerNode)
 
         // connect the player node to the output node
-        audioEngine.connect(playerNode,
+        audioEngine.connect(playerNode1,
+                            to: mixerNode,
+                            format: audioFile1?.processingFormat)
+
+        audioEngine.connect(playerNode2,
+                            to: mixerNode,
+                            format: audioFile2?.processingFormat)
+
+        audioEngine.connect(mixerNode,
                             to: audioEngine.outputNode,
-                            format: audioFile?.processingFormat)
+                            format: audioFile1?.processingFormat)
 
         // schedule the file for full playback
         do {
             try audioEngine.start()
-            playerNode.scheduleFile(audioFile!, at: nil)
-            playerNode.play()
+            playerNode1.scheduleFile(audioFile1!, at: nil)
+            playerNode2.scheduleFile(audioFile2!, at: nil)
+            playerNode1.play()
+            playerNode2.play()
+        
+            
+            // schedule the mixer node instead
+            
         } catch {
             print("Error setting up the audio engine \(error.localizedDescription)")
         }
