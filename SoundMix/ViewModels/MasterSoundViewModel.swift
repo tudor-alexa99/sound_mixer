535e47e37de597851fcb1f55e5f7000066c67b84
tudor_alexa <tudor.alexa99@gmail.com>
2022-03-18 16:09:52 +0200

    Clenup hardcoded nodes

--- SoundMix/ViewModels/MasterSoundViewModel.swift
+++ SoundMix/ViewModels/MasterSoundViewModel.swift
@@ -14,6 +14,9 @@ class MasterSoundViewModel: NSObject, Identifiable, ObservableObject, AVAudioPla
     let currentTimelinePosition: Double = 0.0
     private var audioEngine = AVAudioEngine()
 
+    private var mixerNode = AVAudioMixerNode()
+    private var playerNodesList: [AVAudioPlayerNode] = []
+
     private var audioList: AudioListViewModel = AudioListViewModel()
 
     override init() {
@@ -72,12 +75,58 @@ class MasterSoundViewModel: NSObject, Identifiable, ObservableObject, AVAudioPla
             playerNode2.scheduleFile(audioFile2!, at: nil)
             playerNode1.play()
             playerNode2.play()
-        
-            
+
             // schedule the mixer node instead
-            
+
         } catch {
             print("Error setting up the audio engine \(error.localizedDescription)")
+        }
+    }
+
+    private func attachAllNodes() {
+        audioList.audioList.forEach { _ in
+            let audioPlayerNode = AVAudioPlayerNode()
+            playerNodesList.append(audioPlayerNode)
+
+            // attach each player node to the engine
+            audioEngine.attach(audioPlayerNode)
+        }
+
+        // use the mixer node to combine all the audio nodes
+        audioEngine.attach(mixerNode)
+    }
+
+    private func connectAllNodes() {
+        // get the format to be used on the main mixer node
+        guard audioList.audioList.count > 0 else { return }
+        let format = audioList.audioList[0].audioFile!.processingFormat
+
+        for index in playerNodesList.indices {
+            // connect all the nodes to the mixer node
+            audioEngine.connect(playerNodesList[index],
+                                to: mixerNode,
+                                format: audioList.audioList[index].audioFile?.processingFormat)
+        }
+
+        // connect the mixer node to the mainMixer / output node of the audio engine
+        audioEngine.connect(mixerNode,
+                            to: audioEngine.outputNode,
+                            format: format)
+    }
+
+    private func scheduleAllNodes() {
+        guard audioList.audioList.count > 0 else { return }
+
+        for index in playerNodesList.indices {
+            playerNodesList[index].scheduleFile(audioList.audioList[index].audioFile!, at: nil)
+        }
+    }
+
+    private func playAllScheduledNodes() {
+        guard audioList.audioList.count > 0 else { return }
+
+        for playerNode in playerNodesList {
+            playerNode.play()
         }
     }
 }
