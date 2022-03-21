ce6c062b9d496ca45863b8715d2ccd0841a61173
tudor_alexa <tudor.alexa99@gmail.com>
2022-03-21 17:52:57 +0200

    Add ruler

--- SoundMix/Views/RulerView.swift
+++ SoundMix/Views/RulerView.swift
@@ -5,16 +5,25 @@
 //  Created by Tudor Alexa on 21.03.2022.
 //
 
+import SlidingRuler
 import SwiftUI
 
 struct RulerView: View {
+    @State private var value: Double = 0
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical, showsIndicators: true) {
+            VStack {
+                SlidingRuler(value: $value)
+            }
+            Spacer()
+        }
     }
 }
 
 struct RulerView_Previews: PreviewProvider {
     static var previews: some View {
         RulerView()
+            .previewInterfaceOrientation(.portraitUpsideDown)
     }
 }
---
+++ SoundMix/Views/RulerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RulerViewController.swift
+//  SoundMix
+//
+//  Created by Tudor Alexa on 21.03.2022.
+//
+
+import UIKit
+
+class RulerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
