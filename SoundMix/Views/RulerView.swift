888028061fffa2036b38537067283103408937ec
tudor_alexa <tudor.alexa99@gmail.com>
2022-03-23 17:32:35 +0200

    Add drag gesture

--- SoundMix/Views/RulerView.swift
+++ SoundMix/Views/RulerView.swift
@@ -12,9 +12,13 @@ struct RulerView: View {
     @State private var value: Double = 0
 
     var body: some View {
-        ScrollView(.vertical, showsIndicators: true) {
+        TrackRow()
+    }
+
+    var rulerBody: some View {
+        HStack {
             VStack {
-                SlidingRuler(value: $value)
+                SlidingRuler(value: $value, in: 0 ... Double.infinity)
             }
             Spacer()
         }
@@ -25,5 +29,33 @@ struct RulerView_Previews: PreviewProvider {
     static var previews: some View {
         RulerView()
             .previewInterfaceOrientation(.portraitUpsideDown)
+    }
+}
+
+struct TrackRow: View {
+    @State var location = CGPoint.zero
+    @State var xCoordinate = 0.0
+
+    var body: some View {
+        ZStack {
+            VStack {
+                SoundwaveSlider(value: .constant(0.3), seekCompletion: { _ in })
+            }
+            .border(.bar, width: 10)
+            .position(x: xCoordinate, y: 100)
+            .coordinateSpace(name: "stack")
+            .contentShape(Rectangle())
+            .zIndex(1)
+            .gesture(dragGesture)
+        }
+    }
+
+    var dragGesture: some Gesture {
+        DragGesture()
+            .onChanged { info in
+                location = info.location
+                xCoordinate = info.location.x
+                print(info.location)
+            }
     }
 }
