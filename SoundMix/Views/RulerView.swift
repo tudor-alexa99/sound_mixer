27b736b63ac93f1010bd62bc74521ce3d8d8beea
tudor_alexa <tudor.alexa99@gmail.com>
2022-03-24 17:46:58 +0200

    Add long press and drag gestures

--- SoundMix/Views/RulerView.swift
+++ SoundMix/Views/RulerView.swift
@@ -9,53 +9,71 @@ import SlidingRuler
 import SwiftUI
 
 struct RulerView: View {
-    @State private var value: Double = 0
-
     var body: some View {
         TrackRow()
     }
 
-    var rulerBody: some View {
-        HStack {
-            VStack {
-                SlidingRuler(value: $value, in: 0 ... Double.infinity)
-            }
-            Spacer()
-        }
-    }
+//    var rulerBody: some View {
+//        VStack {
+//            SlidingRuler(value: $value, in: 0 ... Double.infinity)
+//        }
+//    }
 }
 
 struct RulerView_Previews: PreviewProvider {
     static var previews: some View {
         RulerView()
-            .previewInterfaceOrientation(.portraitUpsideDown)
+            .previewInterfaceOrientation(.landscapeRight)
     }
 }
 
 struct TrackRow: View {
+    var body: some View {
+        ScrollView([.horizontal, .vertical]) {
+            ForEach(1 ... 10, id: \.self) { _ in
+                TrackRowUnit()
+                    .background(Color.red)
+            }
+        }
+        .accessibilityRespondsToUserInteraction()
+//            .background(Color.red)
+    }
+}
+
+struct TrackRowUnit: View {
+    @State private var value: Double = 0
     @State var location = CGPoint.zero
     @State var xCoordinate = 0.0
 
     var body: some View {
-        ZStack {
-            VStack {
-                SoundwaveSlider(value: .constant(0.3), seekCompletion: { _ in })
-            }
+        SoundwaveSlider(value: .constant(0.3), seekCompletion: { _ in })
             .border(.bar, width: 10)
-            .position(x: xCoordinate, y: 100)
+            .position(x: xCoordinate, y: self.location.y)
             .coordinateSpace(name: "stack")
             .contentShape(Rectangle())
-            .zIndex(1)
             .gesture(dragGesture)
-        }
+            .simultaneousGesture(longPressGesture)
+            .padding()
     }
 
     var dragGesture: some Gesture {
-        DragGesture()
+        DragGesture(minimumDistance: 30, coordinateSpace: .global)
             .onChanged { info in
                 location = info.location
                 xCoordinate = info.location.x
                 print(info.location)
+            }
+            .onEnded { _ in
+                print("Drag ended")
+            }
+    }
+
+    var longPressGesture: some Gesture {
+        LongPressGesture(minimumDuration: 2, maximumDistance: 1)
+            .sequenced(before: dragGesture)
+            .onEnded { _ in
+                print("Long press")
+//                self.location = position(x: xCoordinate, y: self.location.y) as! CGPoint
             }
     }
 }
